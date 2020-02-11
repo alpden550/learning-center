7689923208d32f1d0ce30671f8082d9b734092d7
alpden550 <alpden550@gmail.com>
2020-02-11 20:06:20 +0300

    Added Flask-Admin.

--- learning_center/blueprints/admin.py
+++ learning_center/blueprints/admin.py
@@ -1,8 +1,3 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
 
 admin_bp = Blueprint('admin', __name__)
-
-
-@admin_bp.route('/')
-def index():
-    return render_template('admin_dashboard.html')
